93adb872c43067a585425402d028a82ed359c8c5
Killerkego <miklos.botos@gmail.com>
2026-04-14 11:30:43 +0200

    Visualization base

--- Aethelgard/Form1.Designer.cs
+++ Aethelgard/Form1.Designer.cs
@@ -28,19 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblPlayerHp = new Label();
+            progressBar1 = new ProgressBar();
+            progressBar2 = new ProgressBar();
+            lblEnemyHp = new Label();
+            rtbLog = new RichTextBox();
+            btnAttack = new Button();
+            btnStartGame = new Button();
             SuspendLayout();
+            // 
+            // lblPlayerHp
+            // 
+            lblPlayerHp.AutoSize = true;
+            lblPlayerHp.Location = new Point(133, 289);
+            lblPlayerHp.Name = "lblPlayerHp";
+            lblPlayerHp.Size = new Size(75, 20);
+            lblPlayerHp.TabIndex = 0;
+            lblPlayerHp.Text = "Te HP-d: -";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(116, 312);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(125, 29);
+            progressBar1.TabIndex = 1;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(510, 312);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(125, 29);
+            progressBar2.TabIndex = 3;
+            // 
+            // lblEnemyHp
+            // 
+            lblEnemyHp.AutoSize = true;
+            lblEnemyHp.Location = new Point(519, 289);
+            lblEnemyHp.Name = "lblEnemyHp";
+            lblEnemyHp.Size = new Size(100, 20);
+            lblEnemyHp.TabIndex = 2;
+            lblEnemyHp.Text = "Ellenség HP: -";
+            lblEnemyHp.Click += lblEnemyHp_Click;
+            // 
+            // rtbLog
+            // 
+            rtbLog.Location = new Point(102, 33);
+            rtbLog.Name = "rtbLog";
+            rtbLog.Size = new Size(549, 71);
+            rtbLog.TabIndex = 4;
+            rtbLog.Text = "";
+            // 
+            // btnAttack
+            // 
+            btnAttack.Location = new Point(369, 392);
+            btnAttack.Name = "btnAttack";
+            btnAttack.Size = new Size(107, 46);
+            btnAttack.TabIndex = 5;
+            btnAttack.Text = "Támadás";
+            btnAttack.UseVisualStyleBackColor = true;
+            btnAttack.Click += btnAttack_Click;
+            // 
+            // btnStartGame
+            // 
+            btnStartGame.Location = new Point(570, 398);
+            btnStartGame.Name = "btnStartGame";
+            btnStartGame.Size = new Size(94, 29);
+            btnStartGame.TabIndex = 6;
+            btnStartGame.Text = "Új Játék";
+            btnStartGame.UseVisualStyleBackColor = true;
+            btnStartGame.Click += btnStartGame_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnStartGame);
+            Controls.Add(btnAttack);
+            Controls.Add(rtbLog);
+            Controls.Add(progressBar2);
+            Controls.Add(lblEnemyHp);
+            Controls.Add(progressBar1);
+            Controls.Add(lblPlayerHp);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblPlayerHp;
+        private ProgressBar progressBar1;
+        private ProgressBar progressBar2;
+        private Label lblEnemyHp;
+        private RichTextBox rtbLog;
+        private Button btnAttack;
+        private Button btnStartGame;
     }
 }
--- Aethelgard/Form1.cs
+++ Aethelgard/Form1.cs
@@ -1,13 +1,70 @@
+using System;
+using System.Windows.Forms;
+using Aethelgard.Controllers;
+using Aethelgard.Models; // Ezt is be kell húzni a Kasztok (ClassType) miatt!
+
 namespace Aethelgard
 {
     public partial class Form1 : Form
     {
+        // 1. ITT HOZZUK LÉTRE A MENEDZSERT!
+        private GameManager _gameManager = new GameManager();
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            // Opcionális: Alapból letilthatjuk a támadás gombot, amíg nem indult el a játék
+            btnAttack.Enabled = false;
+        }
+
+        // 2. AZ ÚJ JÁTÉK INDÍTÁSA GOMB ESEMÉNYE
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            // A 2. mérföldkőhöz egyelőre fixen bedrótozzuk az adatokat (később majd TextBox-ból olvassuk ki)
+            string playerName = "Teszt Hős";
+            ClassType selectedClass = ClassType.NumberMage;
+
+            // Szólunk a menedzsernek, hogy indítsa el a játékot
+            _gameManager.StartNewGame(playerName, selectedClass);
+
+            // Frissítjük a felületet
+            rtbLog.Text = "A játék elkezdődött! Egy Bináris Farkas állja utad.\r\n";
+            btnAttack.Enabled = true; // Bekapcsoljuk a támadás gombot
+            UpdateStatus(); // Kiírjuk a kezdő életerőket
+        }
+
+        // 3. A TÁMADÁS GOMB ESEMÉNYE (Ez a te kódod, kicsit finomítva)
+        private void btnAttack_Click(object sender, EventArgs e)
+        {
+            // Lefuttatjuk a kört a logikában
+            string roundResult = _gameManager.PlayCombatRound();
+
+            // Frissítjük a szöveges naplót
+            rtbLog.Text = roundResult + "\r\n" + rtbLog.Text;
+
+            // Vizuálisan frissítjük az életerőket
+            UpdateStatus();
+
+            // Ellenőrizzük, vége-e a játéknak, hogy letiltsuk a gombot
+            if (_gameManager.CurrentPlayer.IsDead() || _gameManager.TestEnemy.IsDead())
+            {
+                btnAttack.Enabled = false;
+            }
+        }
+
+        // 4. ÁLLAPOTFRISSÍTŐ METÓDUS
+        private void UpdateStatus()
+        {
+            // Kiírjuk az aktuális számokat a felületre
+            lblPlayerHp.Text = $"Te HP-d: {_gameManager.CurrentPlayer.Health}";
+            lblEnemyHp.Text = $"{_gameManager.TestEnemy.Name} HP-ja: {_gameManager.TestEnemy.Health}";
+        }
+
+        private void lblEnemyHp_Click(object sender, EventArgs e)
         {
 
         }
